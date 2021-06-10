70a925ee839e0b324bb9dd16c16d16d651d94662
csomtoo <chukwurahb@gmail.com>
2021-06-09 22:20:13 -0400

    Aded config files for docer, kubernetes and skaffold to auth servie

--- auth/src/index.go
+++ auth/src/index.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/api/users/currentuser", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hi there!")
+	})
+
 	fmt.Println("Listening on port 3000!")
 	http.ListenAndServe("loaclhost:3000", nil)
 
